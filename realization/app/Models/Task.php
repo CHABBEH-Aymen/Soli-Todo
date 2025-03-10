afb7b55aec467e7b1dae6590cd687106a139f6ef
TamraouiAbdelouahab <abdelwahabtamraoui@gmail.com>
2025-03-10 17:47:02 +0000

    CRUD TaskList and Task

--- realization/app/Models/Task.php
+++ realization/app/Models/Task.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['title', 'description', 'status', 'important', 'creationDate', 'dueDate', 'task_list_id'];
+    protected $fillable = ['title', 'description', 'status', 'important', 'creationDate', 'dueDate', 'tasklist_id'];
 
     public function taskList()
     {
